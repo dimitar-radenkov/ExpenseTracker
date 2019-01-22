c0c97ce5297502d46a5cc5483ad00e6368046328
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2019-01-22 06:36:59 +0200

    added unit test

--- ExpenseTracker.Api.Integration.Tests/AuthorizationControllerTest.cs
+++ ExpenseTracker.Api.Integration.Tests/AuthorizationControllerTest.cs
@@ -1,8 +1,12 @@
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using ExpenseTracker.Api.Attributes;
 using ExpenseTracker.Api.Controllers;
 using ExpenseTracker.Api.Models.BindingModels;
 using ExpenseTracker.Api.Services.Contracts;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -12,7 +16,25 @@ namespace ExpenseTracker.Api.Integration.Tests
     public class AuthorizationControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RegisterAsync_ShouldContainsValidateModelAttribute()
+        {
+            //arrange
+            var authService = new Mock<IAuthService>();
+            var controller = new AuthorizationController(authService.Object);
+
+            //act
+            var method = controller.GetType()
+                .GetMethods()
+                .Where(m => m.Name == nameof(controller.RegisterAsync))
+                .Where(m => m.CustomAttributes.Any(a => a.AttributeType == typeof(ValidateModelAttribute)))
+                .FirstOrDefault();
+
+            //assert
+            Assert.IsNotNull(method);
+        }
+
+        [TestMethod]
+        public void RegisterAsync_WithValidBidingModel_ShouldReturnOk()
         {
             //arrange
             var bindingModel = new RegisterBindingModel
@@ -32,12 +54,11 @@ namespace ExpenseTracker.Api.Integration.Tests
             var controller = new AuthorizationController(authService.Object);
 
             //act
-            var result = controller.RegisterAsync(bindingModel)
-                .ConfigureAwait(false)
-                .GetAwaiter();
+            var result = controller.RegisterAsync(bindingModel).Result as OkResult;
 
             //assert
-            Assert.AreEqual(result, result);
+            Assert.IsNotNull(result);
+            Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
         }
     }
 }
